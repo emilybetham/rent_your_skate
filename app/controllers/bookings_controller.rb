c61dc3be06475010af5212b5e8ce0fd6a2af0772
Emily Betham <emily.betham@bath.edu>
2019-02-19 14:54:25 +0100

    added pundit for user and booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,11 +2,14 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @product = Product.find(params[:product_id])
+    authorize @booking
+    authorize @product
     @booking.product = @product
   end
 
   def create
     @booking = Booking.new(booking_params)
+    authorize @booking
     @booking.product = Product.find(params[:product_id])
     if @booking.save
       redirect_to users_dashboard_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def dashboard
-
+    @products = policy_scope(Product)
+    @bookings = policy_scope(Booking)
   end
 end
