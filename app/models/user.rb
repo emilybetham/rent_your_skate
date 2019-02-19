dc5ce3b8b6c0505457408b53872a30bdd7a717db
Emily Betham <emily.betham@bath.edu>
2019-02-19 18:26:20 +0100

    added hello to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   has_many :products
   has_many :bookings
   has_many :bookings_as_owner, through: :products, source: :bookings
