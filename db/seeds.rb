652c8c2c82a54663e7152b326a54ec8f115dc38c
Dania Aflak <dania.aflak@skema.edu>
2019-02-19 14:48:49 +0100

    pb seed reglé

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ users_attributes = [
     password:     'password'
   }
 ]
-
+User.destroy_all
 User.create!(users_attributes)
 
 Product.destroy_all
@@ -29,23 +29,23 @@ puts 'destroyed all'
 products_attributes = [
   {
     name:         'Seb\'s skate',
-    category:     'Skateboard',
+    category:     'skateboard',
     address:      '7 Boundary St, London E2 7JE',
     description:  'A beautiful skate',
     price:        15,
     photo:        'https://images.unsplash.com/photo-1520045892732-304bc3ac5d8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
   },
   {
-    name:         'Anne\'s trotinette',
-    category:     'Trotinette',
+    name:         'Anne\'s scooter',
+    category:     'scooter',
     address:      '19 rue de Turenne, 75004 Paris',
     description:  'Lovely trotinette',
     price:        18,
     photo:        'https://images.unsplash.com/photo-1541614705297-1cec36271936?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
   },
   {
-    name:         'Romain\'s palme',
-    category:     'Palmes',
+    name:         'Romain\'s scooter',
+    category:     'scooter',
     address:      '23 rue de Oberkampf, 75011 Paris',
     description:  'Nice palme in Piccadilly',
     price:        9,
@@ -57,6 +57,6 @@ products_attributes.each do |attribute|
   product = Product.new(attribute)
   product.remote_photo_url = attribute[:photo]
   product.user = User.last
-  product.save
+  product.save!
 end
 # Product.create!(products_attributes)
