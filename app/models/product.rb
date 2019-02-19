6ab4ea40cf3670e3cf8c635affb3b373c58ea994
Julia Brouard <julia.brouard1@gmail.com>
2019-02-19 14:19:27 +0100

    category collection and placeholders added to products/new form

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :category, :photo, :price, :address, presence: true
+  validates :category, inclusion: { in: ["skateboard", "bike", "scooter", "roller skates"] }
   mount_uploader :photo, PhotoUploader
 end
