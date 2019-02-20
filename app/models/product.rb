34f2cb1544c18fc0a4261b4eb2b1a2cbe40baa50
Emily Betham <emily.betham@bath.edu>
2019-02-20 12:22:08 +0100

    changed seed with correct address

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   validates :name, :category, :photo, :price, :address, presence: true
   validates :category, inclusion: { in: ["skateboard", "bike", "scooter", "roller skates"] }
 
-  after_validation :geocode, if: :will_save_change_to_address?
   geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
 end
