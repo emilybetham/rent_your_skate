6933c85f0492a0b6544b089c15c3a544ea19d9f2
Emily Betham <emily.betham@bath.edu>
2019-02-20 11:22:27 +0100

    map with no style

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
   validates :name, :category, :photo, :price, :address, presence: true
   validates :category, inclusion: { in: ["skateboard", "bike", "scooter", "roller skates"] }
+
+  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :address
   mount_uploader :photo, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_164128) do
+ActiveRecord::Schema.define(version: 2019_02_20_092815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_02_19_164128) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
